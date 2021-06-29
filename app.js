6a2454a3da4320c7b3debe3d4569a3a0e15084b5
rpham2234 <71532602+rpham2234@users.noreply.github.com>
2021-06-28 17:01:31 -0700

    Users can see all reviews

--- app.js
+++ app.js
@@ -4,13 +4,23 @@ const app = express()
 //to use handlebars
 var exphbs = require('express-handlebars');
 
+// OUR MOCK ARRAY OF PROJECTS
+let reviews = [
+    { title: "Great Review", movieTitle: "Batman II" },
+    { title: "Awesome Movie", movieTitle: "Titanic" }
+  ]
+
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
-//Routes
+/*/Routes
 app.get('/', (req, res) => {
     res.render('home', { msg: 'Handlebars are Cool!' });
   })
+*/
+app.get('/', (req, res) => {
+    res.render("reviews-index", {reviews: reviews});
+})
 
 app.listen(3000, () => {
   console.log('App listening on port 3000!')
