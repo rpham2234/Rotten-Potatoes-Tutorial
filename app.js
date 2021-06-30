2617d14f8a6dac4b375cb6011721831de9b858e5
rpham2234 <71532602+rpham2234@users.noreply.github.com>
2021-06-30 12:45:06 -0700

    Users can now see single reviews

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ mongoose.connect('mongodb://localhost:27017/rotten-potatoes', { useNewUrlParser:
 app.use(bodyParser.urlencoded({ extended: true }));
 
 //MongoDB model. This is like a class, or a template to store our form data on MongoDB
-const reviews = mongoose.model("Review", {
+const Review = mongoose.model("Review", {
   title: String,
   movieTitle: String,
   description: String,
@@ -38,42 +38,40 @@ app.set('view engine', 'handlebars');
   })
 */
 app.get('/', (req, res) => {
-    res.render("reviews-index", {reviews: reviews});
+  Review.find({}).lean()
+    .then(reviews => {
+      res.render('reviews-index', { reviews: reviews });
+    })
+    .catch(err => {
+      console.log(err);
+    })
 })
 
+//opens a form to create the review
 app.get('/reviews/new', (req, res) => {
   res.render("reviews-new", {});
 })
 
-//create
+//saves review to the database.
 app.post('/reviews', (req, res) => {
   console.log(req.body); //contents that will be put in the model instance
-  reviews.create(req.body).then((review) => {
-    console.log(reviews); //logs the model instance itself.
-    res.redirect("/");
+  Review.create(req.body).then((review) => {
+    console.log(review); //logs the model instance itself.
+    res.redirect(`/reviews/${review._id}`);
   }).catch((err) => {
     console.log(err.message);
   })
   // res.render('reviews-new', {});
 })
 
-// SHOW
+//show single review
 app.get('/reviews/:id', (req, res) => {
-  res.send("im a review")
-  reviews.findById(req.params.id).then((reviews) => {
-    res.render("reviews-show", {review: reviews})
+  Review.findById(req.params.id).lean().then((review) => {
+    res.render("reviews-show", {review: review})
   }).catch((err) => {
     console.log(err.message);
   })
 });
-
-reviews.find()
-  .then(review => {
-
-  })
-  .catch(err => {
-
-  });
 
 app.listen(3000, () => {
   console.log('App listening on port 3000!')
