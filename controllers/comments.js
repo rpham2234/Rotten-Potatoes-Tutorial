bfe0e4fe4900819ebf91f75f7dde888a9a6d6c52
Richard Pham <richardvanpham@outlook.com>
2021-07-11 16:28:40 -0700

    Users can delete comments

--- controllers/comments.js
+++ controllers/comments.js
@@ -1,8 +1,5 @@
-
-
 module.exports = (app) => {
     const Comment = require('../models/comment')
-    // NEW Comment
     // CREATE Comment
     app.post('/reviews/comments', (req, res) => {
         Comment.create(req.body).then((comment) => {
@@ -12,5 +9,15 @@ module.exports = (app) => {
           console.log(err.message);
         });
       });
+
+    // DELETE
+    app.delete('/reviews/comments/:id', function (req, res) {
+      console.log("DELETE comment")
+      Comment.findByIdAndRemove(req.params.id).then((comment) => {
+        res.redirect(`/reviews/${comment.reviewId}`);
+      }).catch((err) => {
+        console.log(err.message);
+      })
+    })
   
   }
