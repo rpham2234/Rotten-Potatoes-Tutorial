4192f0f7d5d0259147a6d11ebe4eddfdf86576e9
Richard Pham <richardvanpham@outlook.com>
2021-07-14 10:16:22 -0700

    Reviews and dates now have timestamps on them

--- models/comment.js
+++ models/comment.js
@@ -2,10 +2,14 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
-const Comment = mongoose.model('Comment', {
+const Comment = mongoose.model('Comment', new Schema ({
   title: String,
   content: String,
   reviewId: { type: Schema.Types.ObjectId, ref: 'Review' }
-});
+},
+{
+  timestamps: true
+}
+))
 
-module.exports = Comment
+module.exports = Comment;``
--- models/review.js
+++ models/review.js
@@ -2,12 +2,16 @@
 const mongoose = require("mongoose")
 
 //MongoDB model. This is like a class, or a template to store our form data on MongoDB
-const Review = mongoose.model("Review", {
+const Review = mongoose.model("Review", new mongoose.Schema({
     title: String,
     movieTitle: String,
     description: String,
     rating: Number
-  })
+  },
+  {
+    timestamps:true
+  }
+  ))
 
 //Exports model
 module.exports = Review
